3d686093b25d30db388922f236853cdfb0ef5468
Shane Moe <ssoemoe@terpmail.umd.edu>
2020-08-02 12:51:36 -0400

    feat: :sparkles: fully functional slackbot

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 require('dotenv').config();
 const { RTMClient } = require("@slack/rtm-api");
+const { WebClient } = require("@slack/web-api");
+
 const rtm = new RTMClient(process.env.BOT_TOKEN);
+const web = new WebClient(process.env.BOT_TOKEN);
 
 rtm.start().catch(console.err);
 
-rtm.on('ready', async () => {
+rtm.on("ready", async () => {
     console.log("Bot started");
-})
+    sendMessage("#general", "I am online");
+});
+
+rtm.on("slack_event", async (eventType, event) => {
+    if (event && event.type === "message") {
+        if (event.text === "!hi") {
+            sendMessage(event.channel, `Hello, <@${event.user}>`); // mentioning the user syntax is <@userId>
+        }
+    }
+});
+
+const sendMessage = async (channel, message) => {
+    await web.chat.postMessage({
+        channel: channel,
+        text: message
+    });
+};
