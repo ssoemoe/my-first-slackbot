a596ed7aaa5302073437069bf4184cc965f0f9aa
Shane Moe <ssoemoe@terpmail.umd.edu>
2020-08-01 23:13:47 -0400

    feat: added rtm client

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 require('dotenv').config();
 const { RTMClient } = require("@slack/rtm-api");
 const rtm = new RTMClient(process.env.BOT_TOKEN);
-console.log(process.env.BOT_TOKEN);
+
+rtm.start().catch(console.err);
+
+rtm.on('ready', async () => {
+    console.log("Bot started");
+})
